3ac32e78aeda715a0660ac2954ade115ba0ad5a8
aparmer1 <aparmer1@my.westga.edu>
2015-02-06 11:31:48 -0500

    Added CreateIncidentForm to repository

---
+++ TechSupport/View/CreateIncidentForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TechSupport.Model;
+using TechSupport.Controller;
+
+
+namespace TechSupport.View
+{
+    public partial class CreateIncidentForm : Form
+    {
+        private CustomersController custController;
+        private ProductsController prodController;
+        public CreateIncidentForm()
+        {
+            InitializeComponent();
+            custController = new CustomersController();
+            prodController = new ProductsController();
+            this.loadCustomerNames();
+            this.loadProductNames();
+        }
+
+        private void loadCustomerNames()
+        {
+            List<Customers> customerList;
+            try
+            {
+                customerList = this.custController.GetAllCustomers();
+                if (customerList.Count > 0)
+                {
+                    Customers customer;
+                    for (int i = 0; i < customerList.Count; i++)
+                    {
+                        customer = customerList[i];
+                        customerComboBox.Items.Add(customer.CustomerName);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("There are no customer names to display.");
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+                this.Close();
+            }
+        }
+
+        private void loadProductNames()
+        {
+            List<Products> productList;
+            try
+            {
+                productList = this.prodController.GetAllProducts();
+                if (productList.Count > 0)
+                {
+                    Products product;
+                    for (int i = 0; i < productList.Count; i++)
+                    {
+                        product = productList[i];
+                        productComboBox.Items.Add(product.ProductName);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("There are no product names to display.");
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+                this.Close();
+            }
+        }
+
+        private void createIncidentBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool IsValidData()
+        {
+            if (Validator.IsPresent(customerComboBox) &&
+                Validator.IsPresent(productComboBox) &&
+                Validator.IsPresent(titleTextBox) &&
+                Validator.IsPresent(descriptionTextBox))
+                return true;
+            else
+                return false;
+        }
+    }
+}
